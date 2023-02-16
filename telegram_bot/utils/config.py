e63532e9f3a674d5195ca745d0764fc2476a44a8
Rredlik <rredlik@mail.ru>
2023-02-16 16:27:30 +0500

    Init project

---
+++ telegram_bot/utils/config.py
@@ -0,0 +1,10 @@
+from abc import ABC
+from typing import Final
+
+
+class TgConfig(ABC):
+    PRICE: Final = 100
+    HELPER_URL: Final = '@Skidikis'
+    BOT_URL: Final = 'https://t.me/okoEmojiBot'
+    VIP_HELP_URL: Final = 'https://telegra.ph/Vip-commands-09-09-2'
+    FREE_HELP_URL: Final = 'https://telegra.ph/Free-commands-09-09'
