b498b2dd96e7442fda7ae03c7f1014ff406a2ef3
Rredlik <rredlik@mail.ru>
2023-02-17 11:53:59 +0500

    Chenge user guide

--- telegram_bot/utils/config.py
+++ telegram_bot/utils/config.py
@@ -3,8 +3,8 @@ from typing import Final
 
 
 class TgConfig(ABC):
-    PRICE: Final = 100
+    PRICE: Final = 50
     HELPER_URL: Final = '@Skidikis'
     BOT_URL: Final = 'https://t.me/okoEmojiBot'
-    VIP_HELP_URL: Final = 'https://telegra.ph/Vip-commands-09-09-2'
-    FREE_HELP_URL: Final = 'https://telegra.ph/Free-commands-09-09'
+    VIP_HELP_URL: Final = 'https://telegra.ph/Vip-commands-02-17'
+    FREE_HELP_URL: Final = 'https://telegra.ph/Free-commands-02-17'
