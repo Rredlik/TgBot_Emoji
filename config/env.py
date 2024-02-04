c80004f298e9e32c9ca8a9100139ee9c7e19e9f7
Rredlik <rredlik@mail.ru>
2024-02-04 20:04:54 +0500

    Update env.py

--- config/env.py
+++ config/env.py
@@ -1,22 +1,23 @@
 import os
 from abc import ABC
 from typing import Final
-from dotenv import load_dotenv
+from environs import Env
 
 from yookassa import Configuration
 
 
 class Env(ABC):
-    load_dotenv()
-    ADMIN_ID: Final = int(os.environ.get("ADMIN_ID", "define me!"))
-    TOKEN: Final = os.environ.get('TOKEN', 'define me!')
-    PAYMENTS_TOKEN: Final = os.environ.get("PAYMENTS_TOKEN", "define me!")
+    env = Env()
+    env.read_env()
+    ADMIN_ID: Final = env.int("ADMIN_ID")  # int(os.environ.get("ADMIN_ID", "define me!"))
+    BOT_TOKEN: Final = env.str('BOT_TOKEN')  # , 'define me!')
+    # PAYMENTS_TOKEN: Final = env.str("PAYMENTS_TOKEN")  # , "define me!")
 
-    API_ID: Final = os.environ.get("API_ID", "define me!")
-    API_HASH: Final = os.environ.get("API_HASH", "define me!")
+    API_ID: Final = env.str("API_ID")  # , "define me!")
+    API_HASH: Final = env.str("API_HASH")  # , "define me!")
 
-    SHOP_ID: Final = os.environ.get("SHOP_ID", "define me!")
-    SHOP_TOKEN: Final = os.environ.get("SHOP_TOKEN", "define me!")
+    # SHOP_ID: Final = env.str("SHOP_ID")  # , "define me!")
+    # SHOP_TOKEN: Final = env.str("SHOP_TOKEN")  # , "define me!")
 
 
-Configuration.configure(account_id=Env.SHOP_ID, secret_key=Env.SHOP_TOKEN)
+# Configuration.configure(account_id=Env.SHOP_ID, secret_key=Env.SHOP_TOKEN)
